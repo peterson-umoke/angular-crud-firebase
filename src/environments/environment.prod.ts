4f86f5546d025c6a79cca67c790aa5c9a4f0ac1f
Peterson Umoke <umoke10@hotmail.com>
2022-07-08 15:51:52 +0100

    added firebase to the mix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-mealprep',
+    appId: '1:1005069315165:web:5bf8baa20b582d279631b4',
+    storageBucket: 'fir-mealprep.appspot.com',
+    apiKey: 'AIzaSyBBkYL23-QIU_IDbccVwdBkDryLUGvLWj8',
+    authDomain: 'fir-mealprep.firebaseapp.com',
+    messagingSenderId: '1005069315165',
+    measurementId: 'G-JSNFRSX7CH',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fir-mealprep',
+    appId: '1:1005069315165:web:5bf8baa20b582d279631b4',
+    storageBucket: 'fir-mealprep.appspot.com',
+    apiKey: 'AIzaSyBBkYL23-QIU_IDbccVwdBkDryLUGvLWj8',
+    authDomain: 'fir-mealprep.firebaseapp.com',
+    messagingSenderId: '1005069315165',
+    measurementId: 'G-JSNFRSX7CH',
+  },
   production: false
 };
 
